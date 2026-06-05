1e45c736c2c6d82d5158d53a0d14642c9f720c3b
Volodya Datsyshyn <imvovo4kaa@gmail.com>
2026-06-05 18:26:15 +0300

    fix: fixed bugs at second time according to mentor's  review

--- services/user.py
+++ services/user.py
@@ -10,20 +10,15 @@ def create_user(
         password: str,
         email: str = None,
         first_name: str = None,
-        last_name: str = None
-) -> Any:
-    user = User.objects.create(username=username)
-    user.set_password(password)
-
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-    user.save()
-    return user
-
+        last_name: str = None,
+) -> User:
+    return User.objects.create_user(
+        username=username,
+        password=password,
+        email=email or "",
+        first_name=first_name or "",
+        last_name=last_name or ""
+    )
 
 def get_user(user_id: int) -> Any:
     return User.objects.get(id=user_id)
